fd9413f977cd13669aa95a3e12ea635ce91b6b76
Roberto Torres <robertocarlostorresgalvan@gmail.com>
2022-02-18 16:43:28 -0600

    Esta es mi primera versión

---
+++ src/pruebaholamundo/PruebaHolaMundo.java
@@ -0,0 +1,13 @@
+package pruebaholamundo;
+
+/**
+ *
+ * @author rober
+ */
+public class PruebaHolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
